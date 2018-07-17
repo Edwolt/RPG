e8644fc970b680ad2f31675be01d821030b24431
Unknown <eduardos.rocha17@gmail.com>
2018-07-17 10:05:35 -0300

    Atualização de Código

--- Modulos/Janela.py
+++ Modulos/Janela.py
@@ -6,14 +6,18 @@ class Janela:
     def __init__(self):
         self.display = None
         self.tela = Jogo()
+        self.fullscreen = False
+        self.tam = 100, 100
 
-    def tamanho(self, largura, altura, fullscreen=False):
+    def tamanho(self, tam, fullscreen=False):
+        self.tam = tam
+        print(f'Resolução: {self.tam}' + ' Fullscreen' if fullscreen else '')
         if fullscreen:
-            self.display = pygame.display.set_mode((largura, altura), pygame.FULLSCREEN)
+            self.fullscreen = True
+            self.display = pygame.display.set_mode(self.tam, pygame.FULLSCREEN)
         else:
-            self.display = pygame.display.set_mode((largura, altura))
-        print(self.display.__class__)
-        self.tela.tamanho(largura, altura)
+            self.display = pygame.display.set_mode(self.tam)
+        self.tela.tamanho(self.tam)
 
     def executar(self):
         self.display.blit(self.tela.surface(), (0, 0))
@@ -25,8 +29,12 @@ class Janela:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+                print('Quit: Fechar')
                 quit(0)
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
+                    print('Quit: <ESC>')
                     quit(0)
+                if event.key == pygame.K_F11:
+                    self.fullscreen = not self.fullscreen
--- Telas/Jogo.py
+++ Telas/Jogo.py
@@ -3,40 +3,42 @@ import pygame
 
 class Jogo:
     def __init__(self):
-        self.tabela = [[None]]
+        self.tab = [[None]]
         self.tam = None
         self.quad = 50, 50  # Dimensões do quadrado
         self.min = 15  # Número mínimo de quadrados (altura e largura)
         self.retorno = None
 
-    def tamanho(self, largura, altura):
-        self.tam = largura, altura
+    def tamanho(self, tam):
+        self.tam = tam
         quad = min(self.tam[0] // self.min, self.tam[1] // self.min)
         self.quad = quad, quad
-        self.tabela = [
+        print(f'Quad_size:{quad}')
+        self.tab = [
             [pygame.Surface(self.quad)] * (self.tam[1] // self.quad[1] + 1)
             for _ in range(self.tam[0] // self.quad[0] + 1)
         ]
+        print(f'Tab: {len(self.tab)}x{len(self.tab[0])}')
         ret0 = self.quad[0] - self.tam[0] % self.quad[0]
         ret1 = self.quad[1] - self.tam[1] % self.quad[1]
-        print(f"ret0 = {self.tam[0]} % {self.quad[0]} = {ret0}")
-        print(f"ret1 = {self.tam[1]} % {self.quad[1]} = {ret1}")
+        print(f"Retorno[0]: {self.tam[0]} % {self.quad[0]} = {ret0}")
+        print(f"Retorno[1]: {self.tam[1]} % {self.quad[1]} = {ret1}")
         self.retorno = ret0 // 2, ret1 // 2
-        print(f'retono = {self.retorno}')
+        print(f'Retorno: {self.retorno}')
 
     def surface(self):
         from random import randint
-        for i in range(len(self.tabela)):
-            for j in range(len(self.tabela[i])):
+        for i in range(len(self.tab)):
+            for j in range(len(self.tab[i])):
                 cor = randint(0, 255), randint(0, 255), randint(0, 255)
-                self.tabela[i][j] = pygame.Surface(self.quad)
-                self.tabela[i][j].fill(cor)
+                self.tab[i][j] = pygame.Surface(self.quad)
+                self.tab[i][j].fill(cor)
 
         tela = pygame.Surface(self.tam)
 
-        for i in range(len(self.tabela)):
-            for j in range(len(self.tabela[i])):
-                pygame.transform.scale(self.tabela[i][j], self.quad)
-                tela.blit(self.tabela[i][j], (self.quad[0] * i - self.retorno[0], self.quad[1] * j - self.retorno[1]))
+        for i in range(len(self.tab)):
+            for j in range(len(self.tab[i])):
+                pygame.transform.scale(self.tab[i][j], self.quad)
+                tela.blit(self.tab[i][j], (self.quad[0] * i - self.retorno[0], self.quad[1] * j - self.retorno[1]))
 
         return tela
